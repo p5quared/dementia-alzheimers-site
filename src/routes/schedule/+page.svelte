1e6c601ef080df49f2bddab0b73ba37338259713
Peter V <98245483+p5quared@users.noreply.github.com>
2022-12-13 00:15:33 -0500

    many changes -added default appointment objects -added cycling between appointments via buttons -fixed google maps key and added google maps marker

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,5 +1,37 @@
 <script>
-    import { GoogleMap } from "@beyonk/svelte-googlemaps";
+    import { GoogleMap, GoogleMapMarker } from "@beyonk/svelte-googlemaps";
+    import { PUBLIC_GOOGLE_MAPS_KEY} from "$env/static/public";
+
+    let appointments = [
+        {
+            date: "Tomorrow, Feb 29, 2023 @10AM",
+            desc: "Annual Checkup",
+            doctor : "Dr. Van Nostrand",
+            hospital : "Bellevue Medical Center",
+            adddress : "462 1st Ave, NY, NY 10016"
+        },
+        {
+            date: "October 31st",
+            desc: "Pray for Life",
+            doctor : "Dr. Doofenshmirrtz",
+            hospital : "NY Prrresbyterian",
+            adddress : "520 E 70th St"
+        },
+        {
+            date: "Friday 13th",
+            desc: "This is Grim",
+            doctor : "Chuky",
+            hospital : "Muhaahhahahahha",
+            adddress : "462 1st Ave, NY, NY 10016"
+        },
+    ]
+    let selectedAppointment = 0;
+    let previousAppointment = null;
+    let appointmentCycle = () => {
+        previousAppointment = selectedAppointment;
+        selectedAppointment += 1;
+        selectedAppointment %= appointments.length;
+    }
 </script>
 
 
@@ -7,29 +39,32 @@
     <h1>Here is your next appointment:</h1>
     <div id="body">
         <div class="map-wrapper">
-            <GoogleMap apiKey="AIzaSyASFy6OyBywv3yG2Z2Whq2KjsgOc07Qirw" zoom={14} center={{lat: 40.765126, lng: -73.964901}} />
+            <GoogleMap apiKey={PUBLIC_GOOGLE_MAPS_KEY} zoom={16} center={{lat: 40.765126, lng: -73.964901}}>
+                <GoogleMapMarker lat={40.765126} lng={-73.964901} />
+            </GoogleMap>
         </div>
-<!--        <div id="directions">-->
-<!--            <img src={map} alt="google maps">-->
-<!--            <p>Click for Directions</p>-->
-<!--        </div>-->
-        <div id="information">
+        <div >
+            <div id="information">
             <ul>
                 <li>
-                    -Tomorrow, February 29, 2023 @10AM
+                    -{appointments[selectedAppointment].date}
                 </li>
                 <li>
-                    -Annual Checkup
+                    -{appointments[selectedAppointment].doctor}
                 </li>
                 <li>
-                    -Dr. Van Nostrand
+                    -{appointments[selectedAppointment].hospital}
                 </li>
                 <li>
-                    -Belle Haven Medical Center<br>
-                    Address Address, NY 11111
+                    -{appointments[selectedAppointment].adddress}
                 </li>
             </ul>
-            <a href="/">Return</a>
+            <button on:click={appointmentCycle} on:keypress={appointmentCycle}>Next Appointment</button>
+                {#if previousAppointment !== null}
+                    <button on:click={() => selectedAppointment = previousAppointment}>Previous Appointment</button>
+                {/if}
+            </div>
+            <a href="/">Return to Home Menu</a>
         </div>
     </div>
 </div>
@@ -49,15 +84,24 @@
     #information {
         background-color: #284b63;
         padding: 1em;
+        margin-bottom: 1em;
         font-size: 32px;
-        height: 360px;
+        width: 500px;
+    }
+    button {
+        background-color: #3C6E71;
+        color: var(--color-offwhite);
+        padding: 0.7em;
+        font-size: 18px;
     }
     a {
-        padding: 0.25em 0.5em;
-        background-color: #3C6E71;
+        padding: 0.5em 0.5em;
+        background-color: #284B63;
         color: var(--color-offwhite);
         border: solid black;
 
+        font-size: 20pt;
+        font-weight: bold;
     }
     li {
         padding-top: 1em;
@@ -67,7 +111,7 @@
     }
     .map-wrapper {
         width: 800px;
-        height: 800px;
+        height: 700px;
     }
     h1 {
         font-size: 54px;
